0d75e88236afa0486d206e615422b146dd97cadb
Jarretier Adrien <jarretier.adrien@gmail.com>
2017-02-27 21:37:47 +0100

    auto formatting

--- visualisationObjetsGit/src/visualisationobjetsgit/VisualisationObjetsGit.java
+++ visualisationObjetsGit/src/visualisationobjetsgit/VisualisationObjetsGit.java
@@ -29,172 +29,164 @@ import views.GitObjectsFilesTreeView;
  */
 public class VisualisationObjetsGit extends Application {
 
-	private Git gitModel;
+    private Git gitModel;
 
-	private GitObjectsFilesTreeView objectsFilesList;
-	private GitObjectFileContentViewTextFlow objectContent;
+    private GitObjectsFilesTreeView objectsFilesList;
+    private GitObjectFileContentViewTextFlow objectContent;
 
-	@Override
-	public void init() {
+    @Override
+    public void init() {
 
-		gitModel = new Git();
+        gitModel = new Git();
 
-		objectsFilesList = new GitObjectsFilesTreeView(gitModel);
-		objectContent = new GitObjectFileContentViewTextFlow(gitModel, objectsFilesList);
+        objectsFilesList = new GitObjectsFilesTreeView(gitModel);
+        objectContent = new GitObjectFileContentViewTextFlow(gitModel, objectsFilesList);
 
-	}
+    }
 
-	/**
-	 * ouvre une fenetre de selection de dossier, le dossier doit etre un depot
-	 * git valide
-	 * 
-	 * @param stage
-	 *            conteneur parent a cette fenetre de selection de dossier
-	 * 
-	 * @return - objet "File" representant le dossier ".git" <br>
-	 *         - ou null si la selection a ete annulee
-	 * 
-	 * @throws exceptions.NotGitRepositoryException
-	 */
-	private void openGitRepository(Stage stage) throws NotGitRepositoryException, DirectoryDoesNotExistException,
-			NotGitDirectoryException, IOException, Exception {
+    /**
+     * ouvre une fenetre de selection de dossier, le dossier doit etre un depot
+     * git valide
+     * 
+     * @param stage
+     *            conteneur parent a cette fenetre de selection de dossier
+     * 
+     * @return - objet "File" representant le dossier ".git" <br>
+     *         - ou null si la selection a ete annulee
+     * 
+     * @throws exceptions.NotGitRepositoryException
+     */
+    private void openGitRepository(Stage stage) throws NotGitRepositoryException, DirectoryDoesNotExistException,
+            NotGitDirectoryException, IOException, Exception {
 
-		DirectoryChooser dc = new DirectoryChooser();
+        DirectoryChooser dc = new DirectoryChooser();
 
-		dc.setInitialDirectory(new File(System.getProperty("user.dir")));
+        dc.setInitialDirectory(new File(System.getProperty("user.dir")));
 
-		File gitRepository = dc.showDialog(stage);
+        File gitRepository = dc.showDialog(stage);
 
-		// DirectoryChooser.showDialog renvoie null en cas d'annulation
-		if (gitRepository == null) {
-			// return null;
-		} else {
-			File gitDirectory = new File(gitRepository, ".git");
+        // DirectoryChooser.showDialog renvoie null en cas d'annulation
+        if (gitRepository == null) {
+            // return null;
+        } else {
+            File gitDirectory = new File(gitRepository, ".git");
 
-			if (!gitDirectory.exists()) {
-				throw new NotGitRepositoryException("Ce n'est pas un depot git valide !");
-			}
+            if (!gitDirectory.exists()) {
+                throw new NotGitRepositoryException("Ce n'est pas un depot git valide !");
+            }
 
-			gitModel.setGitDirectory(gitDirectory);
+            gitModel.setGitDirectory(gitDirectory);
 
-			// leve une exception si le dossier ".git" n'existe pas
-			// autrement dit : si le dossier selectionne n'est pas un depot git
-			// return gitDirectory;
-		}
-	}
+            // leve une exception si le dossier ".git" n'existe pas
+            // autrement dit : si le dossier selectionne n'est pas un depot git
+            // return gitDirectory;
+        }
+    }
 
-	@Override
-	public void start(Stage primaryStage) {
-		primaryStage.setTitle("Visualisation objets git");
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Visualisation objets git");
 
-		// Les elements de la fenetre principale seront disposes
-		// en haut, gauche, droite, bas et centre
-		BorderPane root = new BorderPane();
+        // Les elements de la fenetre principale seront disposes
+        // en haut, gauche, droite, bas et centre
+        BorderPane root = new BorderPane();
 
-		// fenetre principale de taille fixe 1280 x 720p
-		Scene scene = new Scene(root, 1280, 720);
+        // fenetre principale de taille fixe 1280 x 720p
+        Scene scene = new Scene(root, 1280, 720);
 
-		// -----------------------------------------------------------
-		// ---------------------- barre de menu ----------------------
+        // -----------------------------------------------------------
+        // ---------------------- barre de menu ----------------------
+        // barre de menu
+        MenuBar menuBar = new MenuBar();
 
-		// barre de menu
-		MenuBar menuBar = new MenuBar();
+        Menu menuFile = new Menu("Fichier");
+        menuBar.getMenus().add(menuFile);
 
-		Menu menuFile = new Menu("Fichier");
-		menuBar.getMenus().add(menuFile);
+        // element "ouvrir" dans le menu "fichier"
+        MenuItem menuFileOpen = new MenuItem("Ouvrir");
 
-		// element "ouvrir" dans le menu "fichier"
-		MenuItem menuFileOpen = new MenuItem("Ouvrir");
+        menuFileOpen.setOnAction((ActionEvent t) -> {
 
-		menuFileOpen.setOnAction((ActionEvent t) -> {
+            boolean validGitRepo;
+            do {
+                try {
+                    // File gitDir = openGitRepository(primaryStage);
+                    openGitRepository(primaryStage);
 
-			boolean validGitRepo;
-			do {
-				try {
-					// File gitDir = openGitRepository(primaryStage);
-					openGitRepository(primaryStage);
+                    // si openGitRepository renvoie null on ne fait aucun
+                    // traitement
+                    // ( la selection de dossier a ete annulee )
+                    // if( gitDir != null ) {
+                    // objectsFilesList.addListGitObjects( gitDir );
+                    // }
+                    validGitRepo = true;
+                } catch (Exception e) {
 
-					// si openGitRepository renvoie null on ne fait aucun
-					// traitement
-					// ( la selection de dossier a ete annulee )
-					// if( gitDir != null ) {
-					// objectsFilesList.addListGitObjects( gitDir );
-					// }
+                    Alert alert = new Alert(AlertType.ERROR, e.getMessage());
+                    alert.showAndWait();
+                    validGitRepo = false;
 
-					validGitRepo = true;
-				} catch (Exception e) {
+                }
+            } while (!validGitRepo);
 
-					Alert alert = new Alert(AlertType.ERROR, e.getMessage());
-					alert.showAndWait();
-					validGitRepo = false;
+        });
 
-				}
-			} while (!validGitRepo);
+        menuFile.getItems().add(menuFileOpen);
+        // bouton d'aide
+        ;
+        Menu menuAide = new Menu("Aide");
+        menuBar.getMenus().add(menuAide);
 
-		});
+        // element de Aide;
+        MenuItem menuFileOpen2 = new MenuItem("A propos");
 
-		menuFile.getItems().add(menuFileOpen);
+        menuFileOpen2.setOnAction((ActionEvent t) -> {
+            JOptionPane aide1 = new JOptionPane();
+            aide1.showMessageDialog(null, "insérer texte de l'aide", "Aide", JOptionPane.INFORMATION_MESSAGE);
 
-		// bouton d'aide
-		;
-		Menu menuAide = new Menu("Aide");
-		menuBar.getMenus().add(menuAide);
+        });
 
-		// element de Aide;
+        menuAide.getItems().add(menuFileOpen2);
 
-		MenuItem menuFileOpen2 = new MenuItem("A propos");
+        // ajout de la barre de menu dans la fenetre principale
+        root.setTop(menuBar);
 
-		menuFileOpen2.setOnAction((ActionEvent t) -> {
-			JOptionPane aide1 = new JOptionPane();
-			aide1.showMessageDialog(null, "insérer texte de l'aide", "Aide", JOptionPane.INFORMATION_MESSAGE);
+        // ---------------------- barre de menu ----------------------
+        // -----------------------------------------------------------
+        // -----------------------------------------
+        // ----------------- views -----------------
+        root.setLeft(objectsFilesList);
+        root.setCenter(objectContent);
 
-		});
+        // ----------------- views -----------------
+        // -----------------------------------------
+        // code de la barre de recherche * sera réutilisé plus tard *
+        // GridPane grid = new GridPane();
+        // grid.setPadding(new Insets(30, 30, 30, 30));
+        // grid.setVgap(5);
+        // grid.setHgap(5);
+        //
+        // TextField searchField = new TextField();
+        // searchField.setPromptText("Recherche");
+        // GridPane.setConstraints(searchField, 0, 0);
+        // grid.getChildren().add(searchField);
+        //
+        // Button searchButton = new Button("Recherche");
+        // GridPane.setConstraints(searchButton, 1, 0);
+        // grid.getChildren().add(searchButton);
+        //
+        // root.setCenter(grid);
+        primaryStage.setScene(scene);
+        primaryStage.show();
 
-		menuAide.getItems().add(menuFileOpen2);
+    }
 
-		// ajout de la barre de menu dans la fenetre principale
-		root.setTop(menuBar);
-
-		// ---------------------- barre de menu ----------------------
-		// -----------------------------------------------------------
-
-		// -----------------------------------------
-		// ----------------- views -----------------
-
-		root.setLeft(objectsFilesList);
-		root.setCenter(objectContent);
-
-		// ----------------- views -----------------
-		// -----------------------------------------
-
-		// code de la barre de recherche * sera réutilisé plus tard *
-		// GridPane grid = new GridPane();
-		// grid.setPadding(new Insets(30, 30, 30, 30));
-		// grid.setVgap(5);
-		// grid.setHgap(5);
-		//
-		// TextField searchField = new TextField();
-		// searchField.setPromptText("Recherche");
-		// GridPane.setConstraints(searchField, 0, 0);
-		// grid.getChildren().add(searchField);
-		//
-		// Button searchButton = new Button("Recherche");
-		// GridPane.setConstraints(searchButton, 1, 0);
-		// grid.getChildren().add(searchButton);
-		//
-		// root.setCenter(grid);
-
-		primaryStage.setScene(scene);
-		primaryStage.show();
-
-	}
-
-	/**
-	 * @param args
-	 *            the command line arguments
-	 */
-	public static void main(String[] args) {
-		launch(args);
-	}
+    /**
+     * @param args
+     *            the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
 
 }
